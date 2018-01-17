63ba21aa7a96036d50e55bf01b0b6257687cc31e
Aditya Sathe <aditya@Adityas-MacBook-Pro.local>
2018-01-17 13:44:54 +0530

    updated requirements.txt & Dropbox integration

--- Pi Code/Pi Code.py
+++ Pi Code/Pi Code.py
@@ -1,6 +1,7 @@
 import logging, time, smtplib
 import RPi.GPIO as gp
 import os
+from dropbox import Dropbox
 from clarifai import rest
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
@@ -33,8 +34,11 @@ def soundSensor():
 		return dist
 
 if __name__ == '__main__':
-	app = ClarifaiApp(api_key='c0c92d06f75d4ed5a067630dafface26')
+	app = ClarifaiApp(api_key='c0c92d06f75d4ed5a067630dafface26') #clarifai API
+	dbx = dropbox('CYUdRtzfjvAAAAAAAAAAFwaSXZUo4_2S_-jWgKO7wf3Gkd9OrdiL67hf97oHTJ3I') #Dropbox
 	model = app.models.get('general-v1.3') #Use Custom Model?
+
+	#GPIO for SONAR Sensor
 	gp.setmode(gp.BCM)
 	gp.setup(21, gp.OUT)
 	gp.setup(16, gp.OUT)
@@ -44,6 +48,7 @@ if __name__ == '__main__':
 	gp.setup(24, gp.IN)
 	gp.setwarnings(False)
 
+	#Config Files
 	if os.path.isfile('config.p'):
 		config_dict = load(open('config.p', 'rb'))
 	else:
@@ -56,16 +61,21 @@ if __name__ == '__main__':
 		config_dict.update({"LONG":location_long})
 		dump(open('config.p', 'wb'), config_dict)
 
+	#Logging purposes
 	logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 					level=logging.INFO)
-	
+
+	#Dict_to_API used to send API requests
 	dict_to_API = dict()
 	dict_to_API.update({"U_ID":config_dict["U_ID"]}) #dynamic ID or sth
+	
+	#Main Loop, iterated every 15 seconds
 	while True:
 
 		dist = soundSensor()
+		image_name = dict_to_API["UID"] + "_" + str(now()) + '.jpg'
 		os.system("fswebcam -S 30 --no-banner image.jpg")
-		sleep(5) #Maybe 30 secs?
+		sleep(5) #Maybe 30 secs? CHANGE TO 15 for review
 		dict_to_API.update({"LEVEL":config_dict['DEPTH'] - dist})
 		dict_to_API.update({"TIMESTAMP":str(now())})
 		
@@ -73,5 +83,7 @@ if __name__ == '__main__':
 		#image = ClImage(file_obj=open('image.jpg', 'rb'))
 		#tags = model.predict([image])
 		#add tags to dict_to_API
-		#dropbox integration
+		dbx.files_upload(open(image_name, 'rb').read(), '/'+image_name)
+		DROPBOX_URL = str(dbx.sharing_create_shared_link().url)
+		dict_to_API.update({"URL":DROPBOX_URL})
 		api.send_data(dict_to_API)
