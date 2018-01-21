a04b448b9382f2505fb5943d9d54becee0c45e31
Aditya Sathe <aditya@Adityas-MacBook-Pro.local>
2018-01-21 16:41:12 +0530

    Pi Code OOP

--- Pi Code/application.py
+++ Pi Code/application.py
@@ -1,1 +1,9 @@
 #The code here will integrate "Pi Code.py" and Telegram API in a single file
+import PiCode as Pi
+import telegram_api as ta
+
+if __name__ == '__main__':
+	#add multiprocessing
+	ta.poll()
+	pi = Pi()
+	pi.loop()
