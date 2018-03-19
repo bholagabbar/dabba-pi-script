17a038b521dfa9a2faa188a56b140672959e6ed4
Aditya Sathe <aditya@Adityas-MacBook-Pro.local>
2018-03-19 19:54:39 +0530

    bug fixes

--- Pi Code/application.py
+++ Pi Code/application.py
@@ -1,24 +1,5 @@
-#The code here will integrate "Pi Code.py" and Telegram API in a single file
-from multiprocessing import Process, Lock, Value
 import PiCode as pc
-import telegram_api as ta
 
 if __name__ == '__main__':
-
-	lock = Lock()
-	distance = Value('d', 0.0)
-	# URL = Value('s')
-
-	telegram = ta.telegram()
-	
-	# proc_2 = Process(target=telegram.poll)
-
-	
-	# proc_2.start()
-
 	pi = pc.Pi()
-	proc_1 = Process(target=pi.loop)
-	proc_1.start()
-
-	telegram.poll()
-
+	pi.loop()
