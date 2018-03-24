7ed52245c09c5dda45b65a626eedb93e164ae995
sathe-aditya <sathe_aditya@hotmail.com>
2018-03-24 14:06:33 +0530

    formatting

--- Pi Code/vision_module.py
+++ Pi Code/vision_module.py
@@ -1,15 +1,14 @@
 def get_tags(client, types, image):
+    # Loads the image into memory
+    with io.open(image, 'rb') as image_file:
+        content = image_file.read()
 
-	# Loads the image into memory
-	with io.open(image, 'rb') as image_file:
-	    content = image_file.read()
+    image = types.Image(content=content)
 
-	image = types.Image(content=content)
-
-	response = client.label_detection(image=image)
-	labels = response.label_annotations
-	temp = []
-	for label in labels:
-		if label.score > 0.5:
-			temp.append(label.description)
-	return temp
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    temp = []
+    for label in labels:
+        if label.score > 0.5:
+            temp.append(label.description)
+    return temp
