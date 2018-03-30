5132b9f34e83e745339652ddae5ef5d6acbbb37f
sathe-aditya <sathe_aditya@hotmail.com>
2018-03-30 17:51:53 +0530

    gpio test

---
+++ Pi Code/gpio_test.py
@@ -0,0 +1,24 @@
+import time
+
+import RPi.GPIO as gp
+
+gp.setmode(gp.BCM)
+gp.setup(23, gp.OUT)
+gp.setup(24, gp.IN)
+gp.setwarnings(False)
+
+whlie True:
+	gp.output(23, True)
+	time.sleep(0.00001)
+	gp.output(23, False)
+
+	while gp.input(24) == 0:
+		pulseStart = time.time()
+
+	while gp.input(24) == 1:
+		pulseEnd = time.time()
+
+	pulseDur = pulseEnd - pulseStart
+	dist = pulseDur * 17150
+	dist = round(dist, 2)
+	print dist
