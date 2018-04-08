2dd3aebbaa12cd49ebf45482f841098cf80e57a5
sathe-aditya <sathe_aditya@hotmail.com>
2018-04-08 18:01:40 +0530

    strobe

--- Pi Code/lights.py
+++ Pi Code/lights.py
@@ -2,10 +2,10 @@ import time
 import RPi.GPIO as gp
 
 
-def strobe(times=10):
+def strobe(times=10, sec=1):
     while times:
         gp.output(18, True)
-        time.sleep(1)
+        time.sleep(sec)
         gp.output(18, False)
-        time.sleep(1)
+        time.sleep(sec)
     times -= 1
