f966cdebbe088ce36fe5f7876cf708d64431c3b2
sathe-aditya <sathe_aditya@hotmail.com>
2018-04-08 11:57:25 +0530

    pre_processing

--- Pi Code/PiCode.py
+++ Pi Code/PiCode.py
@@ -23,97 +23,95 @@ import api
 # from clarifai_module import get_tags
 
 
-
 class Pi:
-	def __init__ (self):
-		# self.app = ClarifaiApp(api_key='c0c92d06f75d4ed5a067630dafface26') #clarifai API
-		self.dbx = Dropbox('CYUdRtzfjvAAAAAAAAAAFwaSXZUo4_2S_-jWgKO7wf3Gkd9OrdiL67hf97oHTJ3I') #Dropbox
-		# self.model = self.app.models.get('general-v1.3') #Use Custom Model?
-		self.visionClient = vision.ImageAnnotatorClient()
+    def __init__(self):
+        # self.app = ClarifaiApp(api_key='c0c92d06f75d4ed5a067630dafface26') #clarifai API
+        self.dbx = Dropbox('CYUdRtzfjvAAAAAAAAAAFwaSXZUo4_2S_-jWgKO7wf3Gkd9OrdiL67hf97oHTJ3I')  # Dropbox
+        # self.model = self.app.models.get('general-v1.3') #Use Custom Model?
+        self.visionClient = vision.ImageAnnotatorClient()
 
-	#GPIO for SONAR Sensor
-		gp.setmode(gp.BCM)
-		gp.setup(23, gp.OUT)
-		gp.setup(24, gp.IN)
-		gp.setwarnings(False)
+        # GPIO for SONAR Sensor
+        gp.setmode(gp.BCM)
+        gp.setup(23, gp.OUT)
+        gp.setup(24, gp.IN)
+        gp.setwarnings(False)
 
-		try:
-			with open('config.json','r') as json_config_file:
-				config = json.loads(json_config_file.read())
+        try:
+            with open('config.json', 'r') as json_config_file:
+                config = json.loads(json_config_file.read())
 
-			for k, v in config.iteritems():
-				os.environ[k] = v
-			print os.environ['CHARTS_DB_HOST']
-# Config file not passed! Using defaults in local
-		except Exception as e:
-			print e
-			logging.warning('Config file not found. Using defaults with CHARTS_DB_HOST as %s' % (os.environ.get('CHARTS_DB_HOST', None)))
+            for k, v in config.iteritems():
+                os.environ[k] = v
+            print os.environ['CHARTS_DB_HOST']
+        # Config file not passed! Using defaults in local
+        except Exception as e:
+            print e
+            logging.warning('Config file not found. Using defaults with CHARTS_DB_HOST as %s' % (
+                os.environ.get('CHARTS_DB_HOST', None)))
 
-	#Config Files
+    # Config Files
 
+    # Main Loop, iterated every 15 seconds
 
-	#Main Loop, iterated every 15 seconds
+    def loop(self):
 
-	def loop(self):
+        # self.config_dict = load(open('config.p', 'rb'))
+        self.config_dict = dict()
+        self.init_dist = sound_sensor()
+        print self.init_dist
+        self.U_ID = str(get_mac())
+        self.config_dict.update({"U_ID": self.U_ID})
+        self.config_dict.update({"DEPTH": self.init_dist})
 
-		#self.config_dict = load(open('config.p', 'rb'))
-		self.config_dict = dict()
-		self.init_dist = sound_sensor()
-		print self.init_dist
-		self.U_ID = str(get_mac())
-		self.config_dict.update({"U_ID":self.U_ID})
-		self.config_dict.update({"DEPTH":self.init_dist})
+        while api.confirm_authentication(self.U_ID):
+            print "L"
+            pass
 
+        lat, lon = api.get_location(self.U_ID)
+        self.config_dict["LAT"], self.config_dict["LONG"] = lat, lon
+        self.config_dict["USER_NAME"] = str(api.get_username(self.U_ID) + "_2")
+        self.config_dict["TYPE"] = api.get_type(self.U_ID)
+        dump(self.config_dict, open('config.p', 'wb'))
+        # Dict_to_API used to send API requests
+        self.dict_to_API = dict()
+        self.dict_to_API.update({"U_ID": self.config_dict["U_ID"]})
+        self.dict_to_API.update({"LAT": self.config_dict["LAT"]})
+        self.dict_to_API.update({"LONG": self.config_dict["LONG"]})
+        self.dict_to_API.update({"USER_NAME": self.config_dict["USER_NAME"]})
+        self.dict_to_API.update({"TYPE": self.config_dict["TYPE"]})
 
-		while api.confirm_authentication(self.U_ID):
-			print "L"
-			pass
+        i = 0
+        while True:
+            print "Recording Distance"
+            dist = sound_sensor()
+            if dist > self.config_dict['DEPTH']:
+                sleep(5)
+                continue
+            image_name = self.dict_to_API["U_ID"] + "_" + str(datetime.datetime.now()) + '.jpg'
+            print "Capturing Image"
+            os.system("fswebcam -r 1024x768 -S 30 --no-banner -q image_{}.jpg".format(i))
 
-		lat, lon = api.get_location(self.U_ID)
-		self.config_dict["LAT"], self.config_dict["LONG"] = lat, lon
-		self.config_dict["USER_NAME"] = str(api.get_username(self.U_ID)+"_2")
-		self.config_dict["TYPE"] = api.get_type(self.U_ID)
-		dump(self.config_dict, open('config.p', 'wb'))
-		#Dict_to_API used to send API requests
-		self.dict_to_API = dict()
-		self.dict_to_API.update({"U_ID":self.config_dict["U_ID"]})
-		self.dict_to_API.update({"LAT":self.config_dict["LAT"]})
-		self.dict_to_API.update({"LONG":self.config_dict["LONG"]})
-		self.dict_to_API.update({"USER_NAME":self.config_dict["USER_NAME"]})
-		self.dict_to_API.update({"TYPE": self.config_dict["TYPE"]})
+            print "Sleeping for 5 seconds"
+            sleep(5)  # Maybe 30 secs? CHANGE TO 15 for review
 
-		i = 0
-		while True:
-			print "Recording Distance"
-			dist = sound_sensor()
-			if dist > self.config_dict['DEPTH']:
-                            sleep(5)
-			    continue
-			image_name = self.dict_to_API["U_ID"] + "_" + str(datetime.datetime.now()) + '.jpg'
-			print "Capturing Image"
-			os.system("fswebcam -r 1024x768 -S 30 --no-banner -q image_{}.jpg".format(i))
+            self.dict_to_API.update({"LEVEL": int(self.config_dict['DEPTH'] - dist)})
+            self.dict_to_API.update({"TIMESTAMP": str(datetime.datetime.now())})
 
-			print "Sleeping for 5 seconds"
-			sleep(5) #Maybe 30 secs? CHANGE TO 15 for review
+            print "Getting Tags (Experimantal)"
+            # tags = get_tags(ClImage, self.model, 'image.jpg') THIS WAS CLARIFAI
+            # pre_processing("image_{}.jpg".format(i), "image_{}.jpg".format((i+1)%2))
+            # tags = get_tags(self.visionClient, types, 'send_to_vision.jpg'.format(i))
+            tags = get_tags_tf('image_{}.jpg'.format(i))
+            self.dict_to_API.update({"TAGS": tags})
 
-			self.dict_to_API.update({"LEVEL":int(self.config_dict['DEPTH'] - dist)})
-			self.dict_to_API.update({"TIMESTAMP":str(datetime.datetime.now())})
+            print "Uploading Image"
+            DROPBOX_URL = upload(self.dbx, "image_{}.jpg".format(i), image_name)
+            self.dict_to_API.update({"URL": DROPBOX_URL})
 
-			print "Getting Tags (Experimantal)"
-			# tags = get_tags(ClImage, self.model, 'image.jpg') THIS WAS CLARIFAI
-			#pre_processing("image_{}.jpg".format(i), "image_{}.jpg".format((i+1)%2))
-			#tags = get_tags(self.visionClient, types, 'send_to_vision.jpg'.format(i))
-			tags = get_tags_tf('image_{}.jpg'.format(i))
-			self.dict_to_API.update({"TAGS":tags})
-
-			print "Uploading Image"
-			DROPBOX_URL = upload(self.dbx, "image_{}.jpg".format(i), image_name)
-			self.dict_to_API.update({"URL":DROPBOX_URL})
-
-			# print "Distance: {} cms".format(dist)
-			# print "Dropbox URL: {}".format(DROPBOX_URL)
-			print self.dict_to_API
-			print ""
-			print ""
-			i = (i + 1) % 2
-			#api.send_data(json.dumps(self.dict_to_API))
+            # print "Distance: {} cms".format(dist)
+            # print "Dropbox URL: {}".format(DROPBOX_URL)
+            print self.dict_to_API
+            print ""
+            print ""
+            i = (i + 1) % 2
+        # api.send_data(json.dumps(self.dict_to_API))
